32ea79d24c81adc9f67ef4e7e8b51400a5bc4525
nkvz99 <nakovzoran99@gmail.com>
2024-11-29 23:05:51 +0100

    homework

---
+++ JavaScript/TASK1/hw1.js
@@ -0,0 +1,33 @@
+console.log("Connected");
+
+// TASK1
+let convert = ("Convert feet to meters")
+let meters = 0.3048;
+let feet = 92;
+console.log (meters*feet);
+console.log (typeof meters);
+console.log (typeof feet);
+
+// TASK2 
+let area = ("The area of the reactangle is" );
+let sideA = 123;
+let sideB = 24; 
+console.log(`The area of the rectangle is: ${sideA*sideB}`);
+
+// TASK3
+let radius = ("The radius of the circle is")
+let π = 3.14
+let r2 = 22
+console.log(`The radius of the circle is: ${π*r2*r2}`);
+
+// TASK4
+
+let PricePhone = 119.95
+let Phone = 30
+let rate = 0.05
+let totalPrice = PricePhone * Phone
+let tax = totalPrice * rate 
+let fullPrice = totalPrice + tax
+console.log(fullPrice)
+
+
