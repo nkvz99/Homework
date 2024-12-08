487d90f00d633ace6075bb76183fb059a21a4223
nkvz99 <nakovzoran99@gmail.com>
2024-12-08 20:48:25 +0100

    calculateage

---
+++ JavaScript/TASK3/AgeCalculator/scr/script.js
@@ -0,0 +1,27 @@
+console.log("connected");
+
+
+
+function CalculateAge(BirthYear , CurrentYear) {
+    if (Number.isNaN(BirthYear) || BirthYear > CurrentYear) {
+        alert ('Invalid birth year entered. Please try again')
+        console.log("Invalid birth year entered. Please try again")
+    } else  {
+        let age = CurrentYear - BirthYear;
+        if (age === 0) {
+            console.log("You were born this year!")
+        } else if (age === 1) {
+            console.log("You are 1 year old.");
+        }
+            else {
+                console.log(`You are ${age} years old.`)
+            }
+        }
+    }        
+
+let BirthYear = parseInt(prompt("Enter your birth year:"));
+console.log(CalculateAge(Number(BirthYear) ,"BirthYear"))
+let CurrentYear = new Date().getFullYear();
+console.log(CalculateAge(Number(CurrentYear) , "CurrentYear"));
+
+// ovde nikako ne mozam da se snajdam vo ovaa zadaca 
