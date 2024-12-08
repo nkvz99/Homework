c8e8a6446ee8eec5c3904d48f5e666e9331d76be
nkvz99 <nakovzoran99@gmail.com>
2024-12-08 20:49:13 +0100

    calculateage02

--- JavaScript/TASK3/AgeCalculator/scr/script.js
+++ JavaScript/TASK3/AgeCalculator/scr/script.js
@@ -2,26 +2,26 @@ console.log("connected");
 
 
 
-function CalculateAge(BirthYear , CurrentYear) {
+function CalculateAge(BirthYear, CurrentYear) {
     if (Number.isNaN(BirthYear) || BirthYear > CurrentYear) {
-        alert ('Invalid birth year entered. Please try again')
+        alert('Invalid birth year entered. Please try again')
         console.log("Invalid birth year entered. Please try again")
-    } else  {
+    } else {
         let age = CurrentYear - BirthYear;
         if (age === 0) {
             console.log("You were born this year!")
         } else if (age === 1) {
             console.log("You are 1 year old.");
         }
-            else {
-                console.log(`You are ${age} years old.`)
-            }
+        else {
+            console.log(`You are ${age} years old.`)
         }
-    }        
+    }
+}
 
 let BirthYear = parseInt(prompt("Enter your birth year:"));
-console.log(CalculateAge(Number(BirthYear) ,"BirthYear"))
+console.log(CalculateAge(Number(BirthYear), "BirthYear"))
 let CurrentYear = new Date().getFullYear();
-console.log(CalculateAge(Number(CurrentYear) , "CurrentYear"));
+console.log(CalculateAge(Number(CurrentYear), "CurrentYear"));
 
 // ovde nikako ne mozam da se snajdam vo ovaa zadaca 
