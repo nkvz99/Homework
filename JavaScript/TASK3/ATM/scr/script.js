6dfeee68bad8e8d2417ffd5a0b0d9742941da56e
nkvz99 <nakovzoran99@gmail.com>
2024-12-09 21:19:45 +0100

    homework

--- JavaScript/TASK3/ATM/scr/script.js
+++ JavaScript/TASK3/ATM/scr/script.js
@@ -1,24 +1,21 @@
 console.log("Connected")
 
-
-
-let balance = 500;
-
 function withdraw(amount) {
     if (Number.isNaN(amount)) {
         alert('Please enter a valid amount in numbers.');
         let newwithdrawamount = parseInt(prompt('Enter the amount you want to withdraw in numbers:'));
         return withdraw(newwithdrawamount); 
     } else if (amount > balance) {
-        alert('Not enough balance.');
+        alert('You dont have enought monney');
         let newwithdrawamount = parseInt(prompt(`Your balance is ${balance}. Please enter an amount less than or equal to your balance:`));
         return withdraw(newwithdrawamount); 
     } else {
         balance -= amount;
-        return `You withdrew ${amount}. Your remaining balance is ${balance}.`;
+        return `You withdraw ${amount}. Your remaining balance is ${balance}.`;
     }
 }
-
-let withdrawAmount = parseInt(prompt('Enter the amount you want to withdraw:'));
-let result = withdraw(withdrawAmount);
+let balance = 500;
+let withdrawamount = parseInt(prompt('Enter the amount you want to withdraw:'));
+let result = withdraw(withdrawamount);
 alert(result);
+
