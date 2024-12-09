05c5879db0779dcd16be437f025f9f29fb285e78
nkvz99 <nakovzoran99@gmail.com>
2024-12-09 21:31:51 +0100

    homework

--- JavaScript/TASK3/AgeCalculator/scr/script.js
+++ JavaScript/TASK3/AgeCalculator/scr/script.js
@@ -1,31 +1,41 @@
 console.log("connected");
 
-let BirthYear = parseInt(prompt("Enter your birthday"));
-console.log(CalculateAge(Number(BirthYear) , "BirthYear"));
-
-function CalculateAge(BirthYear, CurrentYear) {
-    if (Number.isNaN (BirthYear) || BirthYear > CurrentYear) {
-        alert('Invalid birth year entered. Please try again')
-        console.log("Invalid birth year entered. Please try again")
-    } else {
-        let age = CurrentYear - BirthYear;
-        if (age === 0) {
-            console.log("You were born this year!")
-        } 
-        else {
-            console.log(`You are ${age} years old.`)
-        }
-    }
-}
-// let CurrentTime = new Date();
-// let CurrentYear = CurrentTime().getFullYear();
-// console.log(CalculateAge(Number(CurrentYear), "CurrentYear"));
 // let BirthYear = parseInt(prompt("Enter your birthday"));
+// console.log(CalculateAge(Number(BirthYear) , "BirthYear"));
+
+// function CalculateAge(BirthYear, CurrentYear) {
+//     if (Number.isNaN (BirthYear) || BirthYear > CurrentYear) {
+//         alert('Invalid birth year entered. Please try again')
+//         console.log("Invalid birth year entered. Please try again")
+//     } else {
+//         let age = CurrentYear - BirthYear;
+//         if (age === 0) {
+//             console.log("You were born this year!")
+//         } 
+//         else {
+//             console.log(`You are ${age} years old.`)
+//         }
+//     }
+// }
+// // let CurrentTime = new Date();
+// // let CurrentYear = CurrentTime().getFullYear();
+// // console.log(CalculateAge(Number(CurrentYear), "CurrentYear"));
+// // let BirthYear = parseInt(prompt("Enter your birthday"));
 
 
-CalculateAge(1998,2024)
-CalculateAge(1999,2024)
-CalculateAge(1997,2024)
+// CalculateAge(1998,2024)
+// CalculateAge(1999,2024)
+// CalculateAge(1997,2024)
 
-// Vo ovaa domasna nikako da mi izracuna promptot samo so povikuvanje mi dava vo consola resenie. dokolku moze pomos kade gresam
+function calculateAge () {
+    let birthYear = parseInt(prompt('Enter your year of birth'));
+    let curretYear = 2024;
+    let year = curretYear - birthYear;
+    if (Number.isNaN (birthYear)){
+        alert ('You entered an invalid number.');
+    }else { 
+        alert(`your age is: ${year}`);
+     }
+    }
+calculateAge ()
 
