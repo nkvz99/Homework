16eae2f18c8de1436dfb55fc6c5eea9efa86568c
nkvz99 <nakovzoran99@gmail.com>
2024-12-09 19:50:46 +0100

    up

--- JavaScript/TASK3/AgeCalculator/scr/script.js
+++ JavaScript/TASK3/AgeCalculator/scr/script.js
@@ -1,27 +1,31 @@
 console.log("connected");
 
-
+let BirthYear = parseInt(prompt("Enter your birthday"));
+console.log(CalculateAge(Number(BirthYear) , "BirthYear"));
 
 function CalculateAge(BirthYear, CurrentYear) {
-    if (Number.isNaN(BirthYear) || BirthYear > CurrentYear) {
+    if (Number.isNaN (BirthYear) || BirthYear > CurrentYear) {
         alert('Invalid birth year entered. Please try again')
         console.log("Invalid birth year entered. Please try again")
     } else {
         let age = CurrentYear - BirthYear;
         if (age === 0) {
             console.log("You were born this year!")
-        } else if (age === 1) {
-            console.log("You are 1 year old.");
-        }
+        } 
         else {
             console.log(`You are ${age} years old.`)
         }
     }
 }
+// let CurrentTime = new Date();
+// let CurrentYear = CurrentTime().getFullYear();
+// console.log(CalculateAge(Number(CurrentYear), "CurrentYear"));
+// let BirthYear = parseInt(prompt("Enter your birthday"));
 
-let BirthYear = parseInt(prompt("Enter your birth year:"));
-console.log(CalculateAge(Number(BirthYear), "BirthYear"))
-let CurrentYear = new Date().getFullYear();
-console.log(CalculateAge(Number(CurrentYear), "CurrentYear"));
 
-// ovde nikako ne mozam da se snajdam vo ovaa zadaca 
+CalculateAge(1998,2024)
+CalculateAge(1999,2024)
+CalculateAge(1997,2024)
+
+// Vo ovaa domasna nikako da mi izracuna promptot samo so povikuvanje mi dava vo consola resenie. dokolku moze pomos kade gresam
+
