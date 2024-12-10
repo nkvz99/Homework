580cd728b1cfc0f090ad77e82b32e3193f7916ef
nkvz99 <nakovzoran99@gmail.com>
2024-12-10 20:31:33 +0100

    hoomework

--- JavaScript/TASK3/Converterhumandogyears/scr/script.js
+++ JavaScript/TASK3/Converterhumandogyears/scr/script.js
@@ -1,60 +1,26 @@
 console.log("Connected");
 
-// FUNCTION FOR BOTH
 
-
-// function Dog_HumanConverter(year, sortof) {
-//     if (Number.isNaN(year)) {
-//         alert ('Please enter age in numbers');
-//     }
-//      else if (sortof === "HumanYears") {
-//         let DogYears = year * 7;
-//         return `Your ${sortof} years are equal to ${DogYears} in dog years.`;
-//     } else if (sortof === "DogYears") {
-//         HumanYears = year / 7;
-//         return `Your ${sortof} years are equal to ${HumanYears} in human years.`; 
-//     } else{
-//         alert (`${sortof} is not correct`)
-//     }
-// }
-
-// let HumanYears = parseInt(prompt('Enter your age'));
-// console.log(Dog_HumanConverter(Number(HumanYears), "HumanYears"));
-// let DogYears = parseInt(prompt('Enter your dogs age'));
-// console.log(Dog_HumanConverter(Number(DogYears), "DogYears"));
-
-// SEPARATE FUNCTION
-//    HUMAN CONVERTER
 
 function HumanConverter(year, sortof) {
     if (Number.isNaN(year)) {
-        alert ('Please enter age in numbers');
-
-    } else if (sortof === "HumanYears") {  
+        alert('Please enter the age in numbers.');
+        let newtry = parseInt(prompt('Please enter the age as a number'), 10);
+        return HumanConverter(newtry); 
+    }
+    else if (sortof === "HumanToDog") {
         let DogYears = year * 7;
-        return `Your ${sortof} years are equal to ${DogYears} in dog years.`;
-    } else {
-        alert (`${sortof} is not correct`)
-    }
-}
-let HumanYears = parseInt(prompt('Enter your age'));
-console.log(HumanConverter(Number(HumanYears), "HumanYears"));
-
-// DOG CONVERTER
-
-function DogConverter(year, sortof) {
-    if (Number.isNaN(year)) {
-        alert ('Please enter Dog age in numbers');
-
-    } else if  (sortof === "DogYears") {
+        return `Your ${year} human years are equal to ${DogYears} in dog years.`;
+    } else if (sortof === "DogToHuman") {
         let HumanYears = year / 7;
-        return `Your ${sortof} years are equal to ${HumanYears} in human years.`;
-
+        return `Your ${year} dog years are equal to ${HumanYears} in human years.`;
     } else {
-        alert (`${sortof} is not correct`)
+        return `${sortof} is not a valid conversion type. Please enter either "HumanToDog" or "DogToHuman".`;
     }
 }
-let DogYears = parseInt(prompt('Enter your dogs age'));
-console.log(DogConverter(Number(DogYears), "DogYears"));
 
+let age = parseInt(prompt('Enter the age (in years):'));
+let converter = prompt('Enter the conversion type ("HumanToDog" or "DogToHuman"):');
+let result = HumanConverter(age, converter);
+alert(result);
 
