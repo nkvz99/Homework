64247332d567823319e24ae1fcc2ea5ba570934c
nkvz99 <nakovzoran99@gmail.com>
2024-12-15 18:08:44 +0100

    homework2

--- JavaScript/TASK4/HOMEWORK2/script.js
+++ JavaScript/TASK4/HOMEWORK2/script.js
@@ -1,1 +1,17 @@
 console.log("Connected")
+
+function sumNumbers(numbers){
+    let number = 0;
+    for (let i= 0; i < numbers.length; i++){
+        number += numbers[i];
+    }
+    return number;
+
+}
+
+
+let numbers1 = sumNumbers([3 , 3 , 3 , 3 , 3])
+console.log(numbers1)
+
+let numbers2 = sumNumbers([5 , 5 , 5 , 5 ,5])
+console.log(numbers2)
