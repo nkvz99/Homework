d6f9242cf9996e5eea25e57daf972db119b6c1c2
nkvz99 <nakovzoran99@gmail.com>
2024-12-16 20:52:48 +0100

    hw

--- JavaScript/TASK5/script.js
+++ JavaScript/TASK5/script.js
@@ -1,20 +1,43 @@
 console.log("Connected")
 
-function Animal(name, kind) {
-    let animal = []; 
+// function Animal(name, kind) {
+//     let animal = []; 
 
-    animal.name = name;
-    animal.kind = kind;
+//     animal.name = name;
+//     animal.kind = kind;
 
-    animal.speak = function(message) {
-        console.log(`${this.kind} ${this.name} says: '${message}'`);
-    };
+//     animal.speak = function(message) {
+//         console.log(`${this.kind} ${this.name} says: '${message}'`);
+//     };
 
-    return animal
+//     return animal
+// }
+
+// let dog = Animal("Sharko", "Dog");
+// dog.speak("Hey bro!!!"); 
+
+// let cat = Animal("Lusi" , "Cat")
+// cat.speak("Mjauuuu")
+
+
+
+// PRVIOT PRIMER PRED PREDAVANJETO STO SAM GO IZGUGLAV A VTORIOT E POSLE POSLEDNIOV CAS.
+// DOKOLKU MOZE SAMO OVDE KOJ E POPRAKTICEN PRIMER ZA KORISTENJE I ZOSTO VI BLAGODARAM.
+
+
+
+function Animal2(name, kind){
+    this.name = name;
+    this.kind = kind;
+    this.speak = function(message){
+        console.log(`${this.kind} ${this.name} says '${message}'`);
+
+    } 
+    
 }
+let dog = new Animal2("Sharko" , "Dog");
+dog.speak("Hey bro!!!")
 
-let dog = Animal("Sharko", "Dog");
-dog.speak("Hey bro!!!"); 
-
-let cat = Animal("Lusi" , "Cat")
+let cat = new Animal2("Lusi" , "Cat");
 cat.speak("Mjauuuu")
+
