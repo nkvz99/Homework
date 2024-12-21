a2ecf53a83760d4274b3aeca79d3b5df862e3231
nkvz99 <nakovzoran99@gmail.com>
2024-12-21 17:08:08 +0100

    hw6 fix

--- JavaScript/TASK6/script.js
+++ JavaScript/TASK6/script.js
@@ -1,21 +1,23 @@
 console.log("Connected");
 
-let numbers =[2,4,5,232,23,22];
-let numberList = document.getElementById("numberList")
+let numbers = [2, 4, 5, 232, 23, 22, "asd", 24];
+let numberList = document.getElementById("numberList");
 let sum = 0;
 let equation = "";
 
 for (let i = 0; i < numbers.length; i++) {
-    numberList.innerHTML += `<li>${numbers[i]}</li>`;
-    sum += numbers[i];
-    if (i === 0) {
-        equation = numbers[i];
-    } else {
-        equation += ' + ' + numbers[i];
+    if (typeof numbers[i] === 'number') {
+        numberList.innerHTML += `<li>${numbers[i]}</li>`;
+        sum += numbers[i];
+        if (equation === "") {
+            equation = `${numbers[i]}`; 
+        } else {
+            equation += ` + ${numbers[i]}`;  
+        }
     }
-    
 }
-equation += `=${sum}`
+equation += ` = ${sum}`;
 document.getElementById("sum").textContent = `The sum is: ${sum}`;
 document.getElementById("equation").textContent = `Mathematical Equation: ${equation}`;
+
 
