9cab40077b681a07115c22d93b868e96b2ec2588
nkvz99 <nakovzoran99@gmail.com>
2025-01-17 16:53:51 +0100

    fixed homework

--- JavaScript Advanced/TASK1/script.js
+++ JavaScript Advanced/TASK1/script.js
@@ -1,28 +1,33 @@
 console.log("Connected")
+
 const clickBtn = document.getElementById("clickBtn");
 const divResult = document.getElementById("usersResult");
+
 function users() {
-    fetch("https://jsonplaceholder.typicode.com/users")  
-        .then(function (response) {     
+    fetch("https://jsonplaceholder.typicode.com/users")
+        .then(function (response) {
             console.log(response);
             let usersJSONresult = response.text()
             console.log(usersJSONresult)
             return usersJSONresult
-            // return response.text();
+
         })
         .then(function (result) {
-            console.log(result);
-            let parsedResult = JSON.parse(result);
+            console.log(result)
+            const parsedResult = JSON.parse(result);
             console.log(parsedResult)
-            parsedResult.forEach(user => {
+            parsedResult.forEach((user) => {
                 divResult.innerHTML += `<p><b>Name:</b> ${user.name}</p>`;
                 divResult.innerHTML += `<p><b>Email:</b> ${user.email}</p>`;
                 divResult.innerHTML += `<p><b>Address:</b> ${user.address.street}, ${user.address.suite}, ${user.address.city}, ${user.address.zipcode}</p>`;
                 divResult.innerHTML += `<p><b>Phone:</b> ${user.phone}</p>`;
                 divResult.innerHTML += `<hr>`;
             });
-}
-);
+        })
+        .catch(function (error) {
+            console.error("Error fetching user data:", error);
+            divResult.innerHTML = `<p style="color: red;">An error occurred while fetching the user data. Please try again later.</p>`;
+        });
 }
 clickBtn.addEventListener("click", function () {
     users();
